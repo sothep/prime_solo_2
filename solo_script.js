f02554a547c1a3033c0395a76f33ee3651108586
Paul Sotherland <paul.sotherland@gmail.com>
2015-12-10 11:40:29 -0600

    fixes three bugs in .js file (bonus now calculated on each employee array instead of the array of arrays; calculateSTI(): added call to Math.round to ensure integer values in newArray[2]; getBaseSTI(): changed the return statement to no longer subtract 1.  Also completed hard and pro challenges, including new style.css file.  Don't know why Chrome claims that page is in Sundanese; language specification in index.html did not fix this.

--- solo_script.js
+++ solo_script.js
@@ -1,5 +1,23 @@
 // ! ! !
 // Three Bugs
+/*
+  First bug: at the start of the FOR loop, changed the argument passed to calculateSTI()
+    from array to array[i].
+  Second bug: in calculateSTI(), added call to Math.round() before assigning value to newArray[2].
+  Third bug: in getBaseSTI(), changed the return statement: no longer subtracting 1 from basePercent
+    before returning the value.
+
+  Hard mode: added a new variable, formattedEmployeeStr, initialized to an empty string before the FOR
+    loop.  Created a new nested FOR loop that iterates through each individual employee's information,
+    and builds a comma-separated string from that.  Then, that string is passed into newText via
+    document.createTextNode, and formattedEmployeeStr is reset to an empty string before the next
+    iteration.
+
+  Pro mode: created a new CSS file, "style.css", and linked it to "index.html".  Removed the bullet points
+  by setting ul {list-style-type:none}.  Also changed the font of the individual list items.  
+*/
+
+
 
 var arrayAtticus = ["Atticus", "2405", "47000", 3];
 var arrayJem = ["Jem", "62347", "63500", 4];
@@ -10,17 +28,26 @@ var array = [arrayAtticus, arrayJem, arrayBoo, arrayScout];
 
 //Create variables used to write to the DOM
 var newEl, newText, position;
+var formattedEmployeeStr = ""; //new variable for "Hard Mode" solution
+
 //Capture the position of insertion into the DOM
 position = document.getElementById('content');
 
 //Loop the array, extracting each array and writing information to the DOM
 //Note that the information is not 'clean'
 for(var i = 0; i < array.length; i++){
-	array[i] = calculateSTI(array);
+	array[i] = calculateSTI(array[i]); //changed argument from "array" to "array[i]"
  	newEl = document.createElement('li');
-	newText = document.createTextNode(array[i]);
+  
+  for (var j = 0; j < array[i].length; j++){ //new FOR loop to build formatted employee string
+    formattedEmployeeStr += array[i][j];
+    if (j < array[i].length - 1) formattedEmployeeStr += ", "
+  }
+
+	newText = document.createTextNode(formattedEmployeeStr);//replaced argument array[i] with formattedEmployeeStr
 	newEl.appendChild(newText);
 	position.appendChild(newEl);
+  formattedEmployeeStr = ""; //reset temporary result text for next iteration
 }
 
 function calculateSTI(array1){
@@ -38,7 +65,7 @@ function calculateSTI(array1){
   }
 
   newArray[1] = bonus;
-  newArray[2] = baseSalary * (1.0 + bonus);
+  newArray[2] = Math.round(baseSalary * (1.0 + bonus));//added call to Math.round
   newArray[3] = Math.round(baseSalary * bonus);
   console.log(newArray[0] + " " + newArray[1] + " " + newArray[2] + " " + newArray[3]);
   return newArray;
@@ -63,7 +90,7 @@ function getBaseSTI(reviewScore){
       basePercent = 0.10;
       break;
   }
-  return basePercent - 1;
+  return basePercent;//removed -1 from return statement
 }
 
 function getYearAdjustment(employeeNumber){
